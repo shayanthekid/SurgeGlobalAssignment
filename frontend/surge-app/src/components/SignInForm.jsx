e5c2f57581989dec842d0f8e7b9490721669977d
shayanthekid <sajidfayazhaniff@gmail.com>
2023-08-19 11:54:58 +0530

    Auth done completely, need to add backend and front end validations as well as redirects. Moving on to user genreated content feature

--- frontend/surge-app/src/components/SignInForm.jsx
+++ frontend/surge-app/src/components/SignInForm.jsx
@@ -1,19 +1,38 @@
 import React, { useState } from 'react';
 
 const SignInForm = () => {
-    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
 
-    const handleSubmit = (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
 
-        // Implement your signin logic here, e.g. send data to backend
+        // Create user data object
         const userData = {
-            username,
+            email,
             password,
         };
 
-        console.log('User Data:', userData);
+        try {
+            const response = await fetch('http://localhost:5000/api/users/login', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(userData),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                console.log('Login successful:', data);
+                // Handle successful login here, e.g. redirect user or update state
+            } else {
+                console.error('Login failed');
+                // Handle login failure here, e.g. display an error message
+            }
+        } catch (error) {
+            console.error('Error during login:', error);
+        }
     };
 
     return (
@@ -21,15 +40,15 @@ const SignInForm = () => {
             <h2 className="text-2xl mb-4 font-semibold">Sign In</h2>
             <form onSubmit={handleSubmit}>
                 <div className="mb-4">
-                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-                        Username
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
+                        Email
                     </label>
                     <input
                         className="w-full p-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500"
-                        type="text"
-                        id="username"
-                        value={username}
-                        onChange={(e) => setUsername(e.target.value)}
+                        type="email"
+                        id="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
                         required
                     />
                 </div>
