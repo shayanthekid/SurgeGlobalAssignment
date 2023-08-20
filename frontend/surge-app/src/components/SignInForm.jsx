a7652a57852ae5b4ce86126b788ad1968f304393
shayanthekid <sajidfayazhaniff@gmail.com>
2023-08-20 20:15:18 +0530

    captcha added

--- frontend/surge-app/src/components/SignInForm.jsx
+++ frontend/surge-app/src/components/SignInForm.jsx
@@ -3,6 +3,9 @@ import {
     useSignIn
 } from 'react-auth-kit'
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { useEffect } from 'react';
+
+import { loadCaptchaEnginge, LoadCanvasTemplate, validateCaptcha } from 'react-simple-captcha';
 
 const SignInForm = () => {
     const [email, setEmail] = useState('');
@@ -10,52 +13,67 @@ const SignInForm = () => {
     const signIn = useSignIn()
     const navigate = useNavigate(); // Get the navigate function
 
+    useEffect(() => {
+        loadCaptchaEnginge(6);
+    }, []);
+
+
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        // Create user data object
-        const userData = {
-            email,
-            password,
-        };
+        let userCaptchaValue = document.getElementById('user_captcha_input').value;
 
-        try {
-            const response = await fetch('http://localhost:5000/api/users/login', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(userData),
-            });
+        if (validateCaptcha(userCaptchaValue)) {
+            // Create user data object
+            const userData = {
+                email,
+                password,
+            };
 
-            if (response.ok) {
-                const responseData = await response.json();
-                console.log('Login successful:', responseData);
-                // Handle successful login here, e.g. redirect user or update state
-                if (
-                    signIn({
-                        token: responseData.token,
-                        expiresIn: responseData.expiresIn,
-                        tokenType: "Bearer",
-                        authState: responseData.authUserState,
+            try {
+                const response = await fetch('http://localhost:5000/api/users/login', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(userData),
+                });
 
-                    })
-                ) {
-                    // Redirect or do-something
-                    console.log("Successful");
-                    navigate('/home'); // Use navigate function to redirect
+                if (response.ok) {
+                    const responseData = await response.json();
+                    console.log('Login successful:', responseData);
+                    // Handle successful login here, e.g. redirect user or update state
+                    if (
+                        signIn({
+                            token: responseData.token,
+                            expiresIn: responseData.expiresIn,
+                            tokenType: "Bearer",
+                            authState: responseData.authUserState,
 
+                        })
+                    ) {
+                        // Redirect or do-something
+                        console.log("Successful");
+                        navigate('/home'); // Use navigate function to redirect
+
+                    } else {
+                        // Handle sign-in error
+                    }
                 } else {
-                    // Handle sign-in error
+                    console.error('Login failed');
+                    // Handle login failure here, e.g. display an error message
                 }
-            } else {
-                console.error('Login failed');
-                // Handle login failure here, e.g. display an error message
+            } catch (error) {
+                console.error('Error during login:', error);
             }
-        } catch (error) {
-            console.error('Error during login:', error);
+        } else {
+            alert('Captcha Does Not Match');
+            document.getElementById('user_captcha_input').value = '';
         }
     };
+        
+      
+
 
     return (
         <div className="bg-white rounded-lg shadow-lg p-8">
@@ -85,7 +103,28 @@ const SignInForm = () => {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         required
+                        minLength={8} // Minimum password length
+                        pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$" // Password pattern
+                        title="Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character."
                     />
+                    <p className="text-xs text-gray-600 mt-1">
+                        Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.
+                    </p>
+                </div>
+                <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                        Captcha
+                    </label>
+                    <input
+                        className="w-full p-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500"
+                        placeholder="Enter Captcha Value"
+                        id="user_captcha_input"
+                        name="user_captcha_input"
+                        type="text"
+                    />
+                </div>
+                <div>
+                    <LoadCanvasTemplate />
                 </div>
                 <button
                     className="bg-indigo-500 text-white py-2 px-4 rounded hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
--- frontend/surge-app/src/components/SignUpForm.jsx
+++ frontend/surge-app/src/components/SignUpForm.jsx
@@ -1,60 +1,81 @@
 import  { useState } from 'react';
+import { useEffect } from 'react';
+
 import { useSignIn } from 'react-auth-kit'
 import { useNavigate } from 'react-router-dom'; // Import useNavigate
+import { loadCaptchaEnginge, LoadCanvasTemplate,  validateCaptcha } from 'react-simple-captcha';
+
+
 
 const SignUpForm = () => {
+
     const [username, setUsername] = useState('');
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+   
     const signIn = useSignIn()
     const navigate = useNavigate(); // Get the navigate function
 
+    useEffect(() => {
+        loadCaptchaEnginge(6);
+    }, []);
+    
     const handleSubmit = async (e) => {
         e.preventDefault();
+     
+        let userCaptchaValue = document.getElementById('user_captcha_input').value;
 
-        // Create a user object with the form data
-        const user = {
-            username,
-            email,
-            password,
-        };
+        if (validateCaptcha(userCaptchaValue)) {
+            // Create a user object with the form data
+            const user = {
+                username,
+                email,
+                password,
+            };
 
-        try {
-            const response = await fetch('http://localhost:5000/api/users/register', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(user),
-            });
+            try {
+                const response = await fetch('http://localhost:5000/api/users/register', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify(user),
+                });
 
-            if (response.ok) {
-                const responseData = await response.json(); // Parse response JSON
-                console.log('User registered successfully');
-                if (
-                    signIn({
-                        token: responseData.token,
-                        expiresIn: responseData.expiresIn,
-                        tokenType: "Bearer",
-                        authState: responseData.authUserState,
-                      
-                    })
-                ) {
-                    // Redirect or do-something
-                    console.log("Successful");
-                    navigate('/home'); // Use navigate function to redirect
+                if (response.ok) {
+                    const responseData = await response.json(); // Parse response JSON
+                    console.log('User registered successfully');
+                    if (
+                        signIn({
+                            token: responseData.token,
+                            expiresIn: responseData.expiresIn,
+                            tokenType: "Bearer",
+                            authState: responseData.authUserState,
 
+                        })
+                    ) {
+                        // Redirect or do-something
+                        console.log("Successful");
+                        navigate('/home'); // Use navigate function to redirect
+
+                    } else {
+                        // Handle sign-in error
+                    }
+                    // You can handle successful registration here, such as redirecting the user
                 } else {
-                    // Handle sign-in error
+                    console.error('User registration failed');
+                    // You can handle registration failure here, such as displaying an error message
                 }
-                // You can handle successful registration here, such as redirecting the user
-            } else {
-                console.error('User registration failed');
-                // You can handle registration failure here, such as displaying an error message
+            } catch (error) {
+                console.error('Error registering user:', error);
             }
-        } catch (error) {
-            console.error('Error registering user:', error);
+        } else {
+            alert('Captcha Does Not Match');
+            document.getElementById('user_captcha_input').value = '';
         }
+
+
+    
     };
 
     return (
@@ -98,7 +119,28 @@ const SignUpForm = () => {
                         value={password}
                         onChange={(e) => setPassword(e.target.value)}
                         required
+                        minLength={8} // Minimum password length
+                        pattern="^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$" // Password pattern
+                        title="Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character."
                     />
+                    <p className="text-xs text-gray-600 mt-1">
+                        Password must be at least 8 characters long and contain at least one uppercase letter, one lowercase letter, one digit, and one special character.
+                    </p>
+                </div> 
+                  <div className="mb-4">
+                    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
+                        Captcha
+                    </label>
+                    <input
+                        className="w-full p-2 border rounded shadow-sm focus:outline-none focus:border-indigo-500"
+                        placeholder="Enter Captcha Value"
+                        id="user_captcha_input"
+                        name="user_captcha_input"
+                        type="text"
+                    />
+                </div>
+                <div>
+                    <LoadCanvasTemplate />
                 </div>
                 <button
                     className="bg-indigo-500 text-white py-2 px-4 rounded hover:bg-indigo-600 focus:outline-none focus:shadow-outline"
